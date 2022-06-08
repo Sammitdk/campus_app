d7110867dbe623c1d08e775cddc52d88da113b1c
RajP <rajvardhanpatil002@gmail.com>
2022-06-08 19:42:18 +0530

    student-Login-Fields-function

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,58 +1,103 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: login(),
-));
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: login(),
+  ));
+}
+
+
 class login extends StatelessWidget {
   const login({Key? key}) : super(key: key);
+
+  static String faculty = "faculty";
+  static int a=1;
+  // Widget RadioButtons => ;
+
+  Widget studentLogin() => Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(30,20,30,20),
+        child: TextField(
+          decoration: InputDecoration(
+            icon: Icon(Icons.numbers),
+            label: Text("PRN Number"),
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(30,20,30,20),
+        child: TextField(
+          decoration: InputDecoration(
+            icon: Icon(Icons.password),
+            label: Text("Password"),
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ),
+      Container(child: FloatingActionButton(onPressed: (){}, child: Text("Sign In")))
+    ],
+  );
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[50],
       body: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(left: 15,top: 20),
-                  child: Image.network("assets/images/logo.jpg",height: 100,),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Card(
-                    child: Column(
-                      children: [
-                        Image.network("assets/icons/student_login.gif",),
-                        Text("Student login",)
-                      ],
-                    ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Card(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Image.network(
+                        "assets/icons/student_login.gif",
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text(
+                          "Student login",
+                          style: TextStyle(
+                            fontFamily: 'BubblegumSans',
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Expanded(
+              ),
+              Expanded(
+                child: Container(
+                  width: 100,
+                  // height: 100,
                   child: Card(
-                    child:  Column(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
                       children: [
-                        Image.network("assets/icons/teacher_login.gif",),
+                        Image.network(
+                          "assets/icons/teacher_login.gif",
+                          repeat: ImageRepeat.repeat,
+                        ),
                         Padding(
-                          padding: const EdgeInsets.all(8.0),
+                          padding: const EdgeInsets.all(10.0),
                           child: Text("Faculty Login"),
                         )
                       ],
                     ),
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
+          studentLogin(),
+        ],
       ),
     );
   }
