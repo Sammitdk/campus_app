56101da780d238628051480fd90a88d1b6ab5693
RajP <rajvardhanpatil002@gmail.com>
2022-06-14 19:51:25 +0530

    Merged changes

--- lib/firebase/wrapper.dart
+++ lib/firebase/wrapper.dart
@@ -1,5 +1,4 @@
 import 'package:campus_subsystem/firebase/test.dart';
-import 'package:campus_subsystem/loadingpage.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
