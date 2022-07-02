d166c4ee830b7a09999d716aac1571a5c9a3f56c
Sammit <sammitkhade77@gmail.com>
2022-07-02 17:57:52 +0530

    updated student_profile.dart

--- lib/student/student_profile.dart
+++ lib/student/student_profile.dart
@@ -25,7 +25,7 @@ class _StudentProfileState extends State<StudentProfile> {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
       if(image == null) return;
-      // final imageTemp = File(image.path);
+      final imageTemp = File(image.path);
       final imagePermanent = await saveFilePermanently(image.path);
 
       setState(() => this.image = imagePermanent);
@@ -48,160 +48,160 @@ class _StudentProfileState extends State<StudentProfile> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Stack(
-        children:[
-          SizedBox(
-            height: height,
-              width: width,
-              child:  CustomPaint(
-                painter: CurvePainter(),
-              )),
-           Positioned(
-            top: height/10,
-            left: width/2.5,
-            child: Center(
-              child: Container(
-                  height: 200,
-                  width: 300,
-                  child: ClipOval(
-                      clipBehavior: Clip.antiAlias,
-                      clipper: MyClipper(),
-                      child: image != null ? Image.file(image!) : Lottie.network("https://assets5.lottiefiles.com/packages/lf20_lyp6fz8l.json"),
-                  )
-              )
-            ),
-           ),
-          Positioned(
-            top: height/3.5,
-              width: width/0.55,
-              child: FloatingActionButton(
-                elevation: 0,
-                backgroundColor: Colors.transparent,
-                foregroundColor: Colors.black,
-                onPressed: (){
-                  pickImage();
-                },
-                child: const Icon(Icons.add_photo_alternate_outlined),
-              )),
-          Positioned(
-            top: 30,
-            left: 15,
-            child: Text("${widget.info['Name']['First']} ${widget.info['Name']['Last']}", style: const TextStyle(fontSize: 40,color: Colors.white,fontFamily:'Bold',),),
-          ),
-          Positioned(
-            top: 83,
-            left: 20,
-            child: Text("${widget.info['Email']}",style: const TextStyle(fontSize: 20,color: Colors.white,fontFamily:'Narrow',)),
-          ),
-          Positioned(
-            top: 120,
-            left: 20,
-            child: Text("${widget.info['Mobile']}",style: const TextStyle(fontSize: 20,color: Colors.white,fontFamily:'Narrow',)),
-          ),
-          Container(
-            padding: EdgeInsetsDirectional.only(top: height/2.2,start: 40),
+          children:[
+            SizedBox(
                 height: height,
-                width: width/1.2,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsetsDirectional.only(bottom: 20),
-                      child: Row(
-                          children: [
-                            const Expanded(
-                              flex : 1,
-                              child: Icon(
-                                Icons.location_city_outlined,
-                                size: 50,
-                                color: Colors.blue,
-                              ),
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: Column(
-                                children: [
-                                  const Text("Address",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
-                                  Text("${widget.info['Address']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsetsDirectional.only(bottom: 20),
-                      child: Row(
-                        children: [
-                          const Expanded(
-                            flex : 1,
-                            child: Icon(
-                              Icons.school_outlined,
-                              size: 50,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              children: [
-                                const Text("Trade",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
-                                Text("${widget.info['Branch']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsetsDirectional.only(bottom: 20),
-                      child: Row(
-                        children: [
-                          const Expanded(
-                            flex : 1,
-                            child: Icon(
-                              Icons.cake_outlined,
-                              size: 50,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              children: [
-                                const Text("Birth Date",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
-                                Text("${widget.info['DOB']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsetsDirectional.only(bottom: 20),
-                      child: Row(
-                        children: [
-                          const Expanded(
-                            flex : 1,
-                            child: Icon(
-                              Icons.numbers,
-                              size: 50,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              children: [
-                                const Text("PRN",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
-                                Text("${widget.info['PRN']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                width: width,
+                child:  CustomPaint(
+                  painter: CurvePainter(),
+                )),
+            Positioned(
+              top: height/10,
+              left: width/2.5,
+              child: Center(
+                  child: Container(
+                      height: 200,
+                      width: 300,
+                      child: ClipOval(
+                        clipBehavior: Clip.antiAlias,
+                        clipper: MyClipper(),
+                        child: image != null ? Image.file(image!) : Lottie.network("https://assets5.lottiefiles.com/packages/lf20_lyp6fz8l.json"),
+                      )
+                  )
               ),
+            ),
+            Positioned(
+                top: height/3.5,
+                width: width/0.55,
+                child: FloatingActionButton(
+                  elevation: 0,
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.black,
+                  onPressed: (){
+                    pickImage();
+                  },
+                  child: const Icon(Icons.add_photo_alternate_outlined),
+                )),
+            Positioned(
+              top: 30,
+              left: 15,
+              child: Text("${widget.info['Name']['First']} ${widget.info['Name']['Last']}", style: const TextStyle(fontSize: 40,color: Colors.white,fontFamily:'Bold',),),
+            ),
+            Positioned(
+              top: 83,
+              left: 20,
+              child: Text("${widget.info['Email']}",style: const TextStyle(fontSize: 20,color: Colors.white,fontFamily:'Narrow',)),
+            ),
+            Positioned(
+              top: 120,
+              left: 20,
+              child: Text("${widget.info['Mobile']}",style: const TextStyle(fontSize: 20,color: Colors.white,fontFamily:'Narrow',)),
+            ),
+            Container(
+              padding: EdgeInsetsDirectional.only(top: height/2.2,start: 40),
+              height: height,
+              width: width/1.2,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(bottom: 20),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                          flex : 1,
+                          child: Icon(
+                            Icons.location_city_outlined,
+                            size: 50,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            children: [
+                              const Text("Address",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
+                              Text("${widget.info['Address']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(bottom: 20),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                          flex : 1,
+                          child: Icon(
+                            Icons.school_outlined,
+                            size: 50,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            children: [
+                              const Text("Trade",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
+                              Text("${widget.info['Branch']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(bottom: 20),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                          flex : 1,
+                          child: Icon(
+                            Icons.cake_outlined,
+                            size: 50,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            children: [
+                              const Text("Birth Date",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
+                              Text("${widget.info['DOB']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(bottom: 20),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                          flex : 1,
+                          child: Icon(
+                            Icons.numbers,
+                            size: 50,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Column(
+                            children: [
+                              const Text("PRN",style: TextStyle(fontSize: 15,color: Colors.black,fontFamily:'Narrow',),),
+                              Text("${widget.info['PRN']}",style: const TextStyle(fontSize: 25,color: Colors.black,fontFamily:'Narrow',)),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ]
       ),
       floatingActionButton: SpeedDial(
@@ -246,7 +246,7 @@ class MyClipper extends CustomClipper<Rect>{
   bool shouldReclip(covariant CustomClipper<Rect> oldClipper) {
     return false;
   }
-  
+
 }
 
 class CurvePainter extends CustomPainter {
