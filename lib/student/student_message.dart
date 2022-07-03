9023f8b2489826be52719b5d402842a1c5950867
RajP <rajvardhanpatil002@gmail.com>
2022-07-03 12:51:25 +0530

    Refresh

--- lib/student/student_message.dart
+++ lib/student/student_message.dart
@@ -1,16 +1,16 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
+// import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 
 class StudentMessage extends StatelessWidget {
   final Map<String,dynamic> info;
   StudentMessage({Key? key, required this.info}) : super(key: key);
-  final FirebaseMessaging obj = FirebaseMessaging.instance;
-  void initState(){
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp)async{
-      print(await obj.getToken());
-    });
-  }
+  // final FirebaseMessaging obj = FirebaseMessaging.instance;
+  // void initState(){
+  //   WidgetsBinding.instance.addPostFrameCallback((timeStamp)async{
+  //     print(await obj.getToken());
+  //   });
+  // }
   @override
   Widget build(BuildContext context) {
     return Padding(
