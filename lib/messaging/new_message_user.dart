ff854a2751d4be4633b9ef42a6ccddcd2870cdc2
Sammit <sammitkhade77@gmail.com>
2022-12-05 16:21:52 +0530

    new message added

--- lib/messaging/new_message_user.dart
+++ lib/messaging/new_message_user.dart
@@ -1,53 +1,153 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-import '../redux/reducer.dart';
-
-class NewMessage extends StatelessWidget {
+class NewMessage extends HookWidget {
   const NewMessage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var data = StoreProvider.of<AppState>(context).state;
+    dynamic futureDocReference = useState(FirebaseFirestore.instance
+        .collection("Student_Detail")
+        .orderBy("Name.First")
+        .get());
     return Scaffold(
-      body: FutureBuilder(
-        future: FirebaseFirestore.instance
-            .collection("Student_Detail/${data.prn}")
-            .get(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, i) {
-                QueryDocumentSnapshot x = snapshot.data!.docs[i];
-                return InkWell(
-                  onTap: () {
-                    // Navigator.push(context, MaterialPageRoute(builder: (_) => ShowEvent(x: x,)));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsetsDirectional.all(18),
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 80,
-                      decoration: BoxDecoration(
-                          borderRadius: const BorderRadiusDirectional.only(
-                              topStart: Radius.circular(50),
-                              topEnd: Radius.circular(50),
-                              bottomEnd: Radius.circular(50),
-                              bottomStart: Radius.circular(50)),
-                          color: Colors.blue[100]),
-                      child: Text(
-                        (x['Name']['First']),
-                        textAlign: TextAlign.center,
-                        style:
-                            const TextStyle(fontFamily: "Bold", fontSize: 30),
-                      ),
-                    ),
-                  ),
-                );
-              });
-        },
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Students",
+          style: TextStyle(fontFamily: 'Narrow', fontSize: 30),
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.indigo[300],
       ),
+      body: Stack(
+        children: [
+          Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 60,
+              child: FutureBuilder(
+                future: futureDocReference.value,
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, i) {
+                          QueryDocumentSnapshot x = snapshot.data!.docs[i];
+                          return User(
+                            imageUrl: x['imgUrl'],
+                            name: x['Name'],
+                            branch: x['Branch'],
+                            year: x['Year'],
+                          );
+                        });
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              )),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: TextField(
+                onChanged: (value) {
+                  futureDocReference.value = FirebaseFirestore.instance
+                      .collection("Student_Detail")
+                      .where("Name.First",
+                          isGreaterThanOrEqualTo: value.replaceFirst(
+                              value[0], value[0].toUpperCase()))
+                      .where("Name.First",
+                          isLessThan:
+                              '${value.replaceFirst(value[0], value[0].toUpperCase())}z')
+                      .orderBy("Name.First")
+                      .get();
+                },
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.grey.shade600),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey.shade600,
+                    size: 20,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: const EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.grey.shade100)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class User extends StatelessWidget {
+  final dynamic imageUrl;
+  final dynamic name;
+  final dynamic branch;
+  final dynamic year;
+  const User({
+    Key? key,
+    required this.imageUrl,
+    required this.name,
+    required this.branch,
+    required this.year,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Row(children: <Widget>[
+        CircleAvatar(
+          backgroundImage: imageUrl != ""
+              ? NetworkImage(imageUrl)
+              : const AssetImage("assets/images/profile.gif") as ImageProvider,
+          maxRadius: 30,
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: Container(
+            color: Colors.transparent,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "${name['First']} ${name['Last']}",
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "$year $branch",
+                      style:
+                          TextStyle(fontSize: 15, color: Colors.grey.shade600),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        IconButton(
+            onPressed: () {}, icon: const Icon(Icons.messenger_outline_rounded))
+      ]),
     );
   }
 }
